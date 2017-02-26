6af798e059f1a929c48327abff4a1cd58980ae45
sandstranger <sandstranger@gmail.com>
2017-02-26 12:11:26 +0400

    libgui: SensorManager: be compatible with MTK Lollipop blobs
    Change-Id: I1846c5f3c629293c0f0fc0a6762f6f7b5b9bd9b5
    
    libgui: SensorManager: fix
    
    Change-Id: I76e0ba036ef4e4acc15c9e6f87cb00dd37fae7c9
    
    [Ported to N by @xen0n.]
    
    Conflicts:
    	include/gui/SensorManager.h
    
    # Conflicts:
    #	include/gui/SensorManager.h (reverted from commit 85eb5aa35863841f975a1c1038c84ef5373eec9d)

--- include/gui/SensorManager.h
+++ include/gui/SensorManager.h
@@ -57,12 +57,7 @@ public:
     ssize_t getSensorList(Sensor const* const** list);
     ssize_t getDynamicSensorList(Vector<Sensor>& list);
     Sensor const* getDefaultSensor(int type);
-#ifdef MTK_HARDWARE
-    sp<SensorEventQueue> createEventQueue(String8 packageName, int mode = 0);
-    sp<SensorEventQueue> createEventQueue();
-#else
     sp<SensorEventQueue> createEventQueue(String8 packageName = String8(""), int mode = 0);
-#endif
     bool isDataInjectionEnabled();
 
 private:
@@ -70,12 +65,6 @@ private:
     void sensorManagerDied();
 
     SensorManager(const String16& opPackageName);
-
-
-#ifdef MTK_HARDWARE
-    SensorManager();
-#endif
-
     status_t assertStateLocked();
 
 private:
@@ -89,15 +78,6 @@ private:
     sp<IBinder::DeathRecipient> mDeathObserver;
     const String16 mOpPackageName;
 };
-
-
-#ifdef MTK_HARDWARE
-// be compatible with MTK Lollipop blobs
-extern "C" {
-    extern android::Mutex _ZN7android9SingletonINS_13SensorManagerEE5sLockE;
-    extern SensorManager *_ZN7android9SingletonINS_13SensorManagerEE9sInstanceE;
-}
-#endif  // MTK_HARDWARE
 
 // ----------------------------------------------------------------------------
 }; // namespace android
